0982f89d4c4582e5a3a32d39343a5419e084eee2
krrong <khk9664@naver.com>
2022-06-10 16:15:32 +0900

    각 command에 맞게 파일 읽는 함수 생성
    insertion과 range search는 한 라인에 숫자가 2개 있고, point search는 한 라인에 숫자가 1개 있음 -> 각 command에 맞춰서 읽을 수 있도록 함수 생성

--- btree.cpp
+++ btree.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
+#include <string>
 using namespace std;
 
 // 참고
@@ -72,7 +73,6 @@ public:
 	// 멤버 변수
 	const char* fileName;	// 파일 이름
 	int blockSize;			// 블록 사이즈
-	FILE* filePointer;
 
 	// 생성자
 	BTree(const char* fileName) {
@@ -87,7 +87,7 @@ public:
 			return;
 		}
 
-		this->filePointer = fopen(this->fileName, "wb");
+		FILE* filePointer = fopen(this->fileName, "wb");
 
 		// 처음 creation시 RootBID와 Depth는 0 고정
 		int RootBID = 0;
@@ -120,7 +120,66 @@ public:
 	int* search(int startRange, int endRange) {
 
 	}
+
+
 };
+
+vector<int> readFile(string readFileName, char command) {
+	FILE* filePointer = fopen(readFileName.c_str(), "r");
+	char str[100];
+	string preStr;	// 마지막 행이 반복되는 것 방지위함
+	vector<int> result;	
+
+	// point search는 한 라인에 하나의 숫자가 있어 분할 필요 X
+	if (command == 's') {
+		while (feof(filePointer) == 0) {
+			fgets(str, 100, filePointer);
+
+			// 개행문자 제거
+			if (str[strlen(str) - 1] == '\n') {
+				str[strlen(str) - 1] = '\0';
+			}
+
+			// 입력이 없으면 종료
+			if (str[0] == '\0') {
+				break;
+			}
+
+			// 읽은 라인을 벡터에 저장
+			result.push_back(stoi(string(str)));	// string -> int
+		}
+	}
+	// range search와 insertion은 한 라인에 두 개의 숫자가 있어 분할 필요
+	else {
+		// 파일 포인터가 파일의 끝이 아닐 때 계속 반복
+		while (feof(filePointer) == 0) {
+			fgets(str, 100, filePointer);
+
+			// 개행문자 제거
+			if (str[strlen(str) - 1] == '\n') {
+				str[strlen(str) - 1] = '\0';
+			}
+
+			char* context = NULL;	// 분리된 후 남은 문자열이 들어감
+			char* token = strtok_s(str, ", ", &context);
+
+			// 마지막 행이 반복되는 것 방지위함
+			if (preStr == token) {
+				break;
+			}
+			preStr = token;
+
+			// 읽은 라인을 토큰화하여 벡터에 저장
+			while (token != NULL) {
+				result.push_back(stoi(string(token)));	// string -> int
+				token = strtok_s(NULL, ", ", &context);
+			}
+		}
+	}
+
+	fclose(filePointer);
+	return result;
+}
 
 // input example
 // 1. btree.exe		c	btree.bin		36
@@ -128,10 +187,12 @@ public:
 // 3. btree.exe		s	btree.bin		search.txt		output.txt
 // 4. btree.exe		r	btree.bin		rangesearch.txt output.txt
 // 5. btree.exe		p	btree.bin		input.txt		output.txt
-//		[0]			[1]		[2]			[3]					[4] 
+//	[0]		[1]		[2]		[3]		[4] 
 int main(int argc, char* argv[]) {
 	char command = argv[1][0];
 	const char* fileName = argv[2];
+	vector<int> data;
+	string readFileName = "";
 
 	BTree* myBTree = new BTree(fileName);
 	int blockSize = 0;
@@ -141,17 +202,37 @@ int main(int argc, char* argv[]) {
 		// create index file
 		myBTree->creation(fileName, blockSize);
 		break;
-
 	case 'i':
 		// insert records from [records data file], ex) records.txt
+		readFileName = argv[3];
+		data = readFile(readFileName, command);
+
+		// 확인
+		for (int i = 0; i < data.size(); i = i + 2) {
+			cout << data[i] << ", " << data[i + 1] << endl;
+		}
 		break;
 
 	case 's':
 		// search keys in [input file] and print results to [output file]
+		readFileName = argv[3];
+		data = readFile(readFileName, command);
+
+		// 확인
+		for (int i = 0; i < data.size(); i = i++) {
+			cout << data[i] << endl;
+		}
 		break;
 
 	case 'r':
 		// search keys in [input file] and print results to [output file]
+		readFileName = argv[3];
+		data = readFile(readFileName, command);
+
+		// 확인
+		for (int i = 0; i < data.size(); i = i + 2) {
+			cout << data[i] << ", " << data[i + 1] << endl;
+		}
 		break;
 
 	case 'p':
